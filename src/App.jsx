1e73338a35a50f628077df5fa4ef08ae74fc38c5
joehobos <joehobos@DESKTOP-A520OBQ>
2023-07-31 18:43:35 -0700

    pokemon-List

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,93 @@
-import { useState } from "react";
+import { useState } from 'react'
+import { useEffect } from 'react'
+import React from 'react'
+import PokemonList from './components/PokemonList'
+import axios from 'axios'
+import Page from './components/Page'
+import './index.css'
 
-const Color = ({color, selectedColor, setSelectedColor}) => {
+
+function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [allPokemon, setAllPokemon] = useState([])
+  const [currentPageUrl, setcurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [previousPageUrl, setpreviousPageUrl] = useState()
+  const [nextPageUrl, setnextPageUrl] = useState()
+  const [loading, setLoading] = useState(true)
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(currentPageUrl).then((response) => {
+      setLoading(false);
+      const pokemonData = response.data.results.map((pokemon) => ({
+        name: pokemon.name,
+        url: pokemon.url,
+        weight: 0,
+        sprite: '', 
+      }));
+      setPokemon(pokemonData)
+      setAllPokemon(pokemonData)
+      setpreviousPageUrl(response.data.previous)
+      setnextPageUrl(response.data.next)
+
+      
+      Promise.all(
+        pokemonData.map(async (poke) => {
+          const response = await axios.get(poke.url);
+          const weight = response.data.weight;
+          const sprite = response.data.sprites.front_default; 
+          return { ...poke, weight, sprite };
+        })
+      ).then((updatedPokemon) => {
+        setPokemon(updatedPokemon);
+      });
+    });
+  }, [currentPageUrl]);
+
+  function gotoPreviousPage() {
+    setcurrentPageUrl(previousPageUrl);
+  }
+
+  function gotoNextPage() {
+    setcurrentPageUrl(nextPageUrl);
+  }
+
+  
+document.addEventListener('DOMContentLoaded', () => {
+  const cards = document.querySelectorAll('.card');
+  cards.forEach((card) => {
+    card.addEventListener('click', () => {
+      card.classList.toggle('enlarged');
+    });
+  });
+});
+
+
+  if (loading === true) {
+    return "Loading...";
+  }
+
+
   return (
-    <div 
-      className={
-        color === selectedColor ? `${color} selected`: color
-      } 
-      onClick={() => setSelectedColor(color)} 
-     ></div>
-    
-  )
-}
-const App = () => {
-  const [selectedColor, setSelectedColor] = useState("")
- 
-  return (
-    <div id="container">
-      <div id="navbar">
-        <div>Currently selected: </div>
-        <div className={selectedColor}>{selectedColor}</div>
-      </div>
-      <div id="colors-list">
-        <Color color ="violet" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
-        <Color color ="blue" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
-        <Color color ="red" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
-      </div>
- 
+    <>
+    <header className='header'>Pokedex</header>
+    <div className='search-container'>
+    <input
+      className='search'
+      type="text"
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+      placeholder="Search Pokemon"
+    />
     </div>
+    <PokemonList pokemon={pokemon} search={search} />
+    <Page
+      gotoPreviousPage={previousPageUrl ? gotoPreviousPage : null}
+      gotoNextPage={nextPageUrl ? gotoNextPage : null}
+    />
+  </>
   );
-};
+}
 
 export default App;
